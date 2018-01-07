953f3d1579fcb7cca3d5d6da4b7052292243d8ce
bernar_1 <alexandre.bernard@epita,fr>
2018-01-07 20:09:48 +0100

    [INFO_REGS]: print all struct user_regs_struct

--- src/commands/info_regs.c
+++ src/commands/info_regs.c
@@ -1,54 +1,51 @@
+#include <err.h>
 #include <stdio.h>
+#include <sys/user.h>
 
 #include "commands.h"
 #include "my_dbg.h"
 #include "dproc.h"
 
-struct reg {
-    char *name;
-    unw_regnum_t no;
-};
-
-static struct reg regs[] = {
-    { "rip", UNW_X86_64_RIP },
-    { "rsp", UNW_X86_64_RSP },
-    { "rbp", UNW_X86_64_RBP },
-    { "rax", UNW_X86_64_RAX },
-    { "rdx", UNW_X86_64_RDX },
-    { "rcx", UNW_X86_64_RCX },
-    { "rbx", UNW_X86_64_RBX },
-    { "rsi", UNW_X86_64_RSI },
-    { "rdi", UNW_X86_64_RDI },
-    { "r8", UNW_X86_64_R8 },
-    { "r9", UNW_X86_64_R9 },
-    { "r10", UNW_X86_64_R10 },
-    { "r11", UNW_X86_64_R11 },
-    { "r12", UNW_X86_64_R12 },
-    { "r13", UNW_X86_64_R13 },
-    { "r14", UNW_X86_64_R14 },
-    { "r15", UNW_X86_64_R15 },
-};
-
 #define array_size(t) (sizeof(t) / sizeof(*t))
+
+#define print_reg(regs, reg)                                   \
+    printf("%s: 0x%llx\n", #reg, regs.reg)
 
 int print_regs(struct dproc *proc)
 {
-    if (unw_init_remote(&proc->unw.c, proc->unw.as,
-                        proc->unw.ui) < 0) {
-        fprintf(stderr, "Error while initing remote\n");
+    struct user_regs_struct regs;
+    if (ptrace(PTRACE_GETREGS, proc->pid, 0, &regs) == -1) {
+        warn("Could not get registers");
         return -1;
     }
 
-    unw_word_t valp;
-    size_t size = array_size(regs);
-    for (size_t i = 0; i < size; ++i) {
-        if (unw_get_reg(&proc->unw.c, regs[i].no, &valp) < 0) {
-            fprintf(stderr, "Error while getting %s", regs[i].name);
-            return -1;
-        }
-
-        printf("%s: 0x%lx\n", regs[i].name, valp);
-    }
+    print_reg(regs, rip);
+    print_reg(regs, rsp);
+    print_reg(regs, rbp);
+    print_reg(regs, eflags);
+    print_reg(regs, orig_rax);
+    print_reg(regs, rax);
+    print_reg(regs, rbx);
+    print_reg(regs, rcx);
+    print_reg(regs, rdx);
+    print_reg(regs, rdi);
+    print_reg(regs, rsi);
+    print_reg(regs, r8);
+    print_reg(regs, r9);
+    print_reg(regs, r10);
+    print_reg(regs, r11);
+    print_reg(regs, r12);
+    print_reg(regs, r13);
+    print_reg(regs, r14);
+    print_reg(regs, r15);
+    print_reg(regs, cs);
+    print_reg(regs, ds);
+    print_reg(regs, es);
+    print_reg(regs, fs);
+    print_reg(regs, gs);
+    print_reg(regs, ss);
+    print_reg(regs, fs_base);
+    print_reg(regs, gs_base);
 
     return 0;
 }
